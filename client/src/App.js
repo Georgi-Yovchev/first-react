98c5de8656084a90987b97cfce5a12bda3f75a48
Georgi-Yovchev <130495193+Georgi-Yovchev@users.noreply.github.com>
2023-04-12 23:39:12 +0300

    add createItem route to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import { CreateItem } from "./components/CreateItem/CreateItem";
 import { Header } from "./components/Header/Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <>
             <Header />
-            <CreateItem />
+            <main>
+                <Routes>
+                    <Route path="/items/create" element={<CreateItem />} />
+                </Routes>
+            </main>
         </>
     );
 }
