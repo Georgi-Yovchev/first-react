b9cbcd9f43396711ea1a895269a0c0ac75c03ac9
Georgi-Yovchev <130495193+Georgi-Yovchev@users.noreply.github.com>
2023-04-13 23:33:22 +0300

    changed homepage background

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,27 +1,6 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 
-// export const Header = () => {
-//     return (
-//         <nav className={styles["one"]}>
-//             <ul>
-//                 <li>
-//                     <a href="/">Home</a>
-//                 </li>
-//                 <li>
-//                     <a href="/about">About</a>
-//                 </li>
-//                 <li>
-//                     <a href="/marketplace">Markerplace</a>
-//                 </li>
-//                 <li>
-//                     <a href="/contact">Contact</a>
-//                 </li>
-//             </ul>
-//         </nav>
-//     );
-// };
-
 export const Header = () => {
     return (
         <nav className={styles["one"]}>
