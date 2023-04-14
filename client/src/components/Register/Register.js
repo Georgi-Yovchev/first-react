bd673b873b4ec1b7079b8dd61a25e3ccb6f1ecaa
Georgi-Yovchev <130495193+Georgi-Yovchev@users.noreply.github.com>
2023-04-14 16:56:44 +0300

    Register css fix

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -2,9 +2,9 @@ import styles from "./Register.module.css";
 
 export const Register = () => {
     return (
-        <main className={styles["create-car"]}>
-            <section className={styles["create-section"]}>
-                <div className={styles["create-form"]}>
+        <main className={styles["register"]}>
+            <section className={styles["register-section"]}>
+                <div className={styles["register-form"]}>
                     <div>
                         <form action="index.html" method="post">
                             <h1> Sign Up </h1>
