d7033f780499501d6ac0bd6200df232380208b1e
Georgi-Yovchev <130495193+Georgi-Yovchev@users.noreply.github.com>
2023-04-15 00:48:39 +0300

    fix catalog css

--- client/src/components/CatalogList/CatalogList.js
+++ client/src/components/CatalogList/CatalogList.js
@@ -3,256 +3,27 @@ import styles from "./CatalogList.module.css";
 
 export const CatalogList = () => {
     return (
-        <>
-            <div className={styles["listing-section"]}>
-                <div className={styles["product"]}>
-                    <div className={styles["image-box"]}>
-                        <div className={styles["images"]} id="image-1" />
-                    </div>
-                    <div className={styles["text-box"]}>
-                        <h2 className="item">Orange</h2>
-                        <h3 className="price">$4.99</h3>
-                        <p className="description">
-                            A bag of delicious oranges!
-                        </p>
-                        <button type="button" name="item-1-button">
-                            Details
-                        </button>
-                    </div>
+        // <main className={styles["catalogList"]}>
+        <section className={styles["listing-section"]}>
+            <div className={styles["product"]}>
+                <div className={styles["image-box"]}>
+                    <div
+                        className={styles["images"]}
+                        style={{
+                            backgroundImage: `url(https://sc01.alicdn.com/kf/UT8.CaXX2NXXXagOFbXC/fresh-navel-oranges.jpg)`,
+                        }}
+                    />
                 </div>
-                <div className="product">
-                    <div className="image-box">
-                        <div className="images" id="image-2" />
-                    </div>
-                    <div className="text-box">
-                        <h2 className="item">Apple</h2>
-                        <h3 className="price">$4.99</h3>
-                        <p className="description">
-                            A bag of delicious apples!
-                        </p>
-                        <label htmlFor="item-2-quantity">Quantity:</label>
-                        <input
-                            type="text"
-                            name="item-2-quantity"
-                            id="item-2-quantity"
-                            defaultValue={1}
-                        />
-                        <button
-                            type="button"
-                            name="item-2-button"
-                            id="item-2-button"
-                        >
-                            Add to Cart
-                        </button>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="image-box">
-                        <div className="images" id="image-3" />
-                    </div>
-                    <div className="text-box">
-                        <h2 className="item">Passionfruit</h2>
-                        <h3 className="price">$4.99</h3>
-                        <p className="description">
-                            A bag of delicious passionfruit!
-                        </p>
-                        <label htmlFor="item-3-quantity">Quantity:</label>
-                        <input
-                            type="text"
-                            name="item-3-quantity"
-                            id="item-3-quantity"
-                            defaultValue={1}
-                        />
-                        <button
-                            type="button"
-                            name="item-3-button"
-                            id="item-3-button"
-                        >
-                            Add to Cart
-                        </button>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="image-box">
-                        <div className="images" id="image-4" />
-                    </div>
-                    <div className="text-box">
-                        <h2 className="item">Pineapple</h2>
-                        <h3 className="price">$4.99</h3>
-                        <p className="description">
-                            A bag of delicious pineapples!
-                        </p>
-                        <label htmlFor="item-4-quantity">Quantity:</label>
-                        <input
-                            type="text"
-                            name="item-4-quantity"
-                            id="item-4-quantity"
-                            defaultValue={1}
-                        />
-                        <button
-                            type="button"
-                            name="item-4-button"
-                            id="item-4-button"
-                        >
-                            Add to Cart
-                        </button>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="image-box">
-                        <div className="images" id="image-5" />
-                    </div>
-                    <div className="text-box">
-                        <h2 className="item">Mango</h2>
-                        <h3 className="price">$4.99</h3>
-                        <p className="description">
-                            A bag of delicious mangos!
-                        </p>
-                        <label htmlFor="item-5-quantity">Quantity:</label>
-                        <input
-                            type="text"
-                            name="item-5-quantity"
-                            id="item-5-quantity"
-                            defaultValue={1}
-                        />
-                        <button
-                            type="button"
-                            name="item-5-button"
-                            id="item-5-button"
-                        >
-                            Add to Cart
-                        </button>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="image-box">
-                        <div className="images" id="image-6" />
-                    </div>
-                    <div className="text-box">
-                        <h2 className="item">Coconut</h2>
-                        <h3 className="price">$4.99</h3>
-                        <p className="description">
-                            A bag of delicious coconuts!
-                        </p>
-                        <label htmlFor="item-6-quantity">Quantity:</label>
-                        <input
-                            type="text"
-                            name="item-6-quantity"
-                            id="item-6-quantity"
-                            defaultValue={1}
-                        />
-                        <button
-                            type="button"
-                            name="item-6-button"
-                            id="item-6-button"
-                        >
-                            Add to Cart
-                        </button>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="image-box">
-                        <div className="images" id="image-7" />
-                    </div>
-                    <div className="text-box">
-                        <h2 className="item">Banana</h2>
-                        <h3 className="price">$4.99</h3>
-                        <p className="description">
-                            A bag of delicious bananas!
-                        </p>
-                        <label htmlFor="item-7-quantity">Quantity:</label>
-                        <input
-                            type="text"
-                            name="item-7-quantity"
-                            id="item-7-quantity"
-                            defaultValue={1}
-                        />
-                        <button
-                            type="button"
-                            name="item-7-button"
-                            id="item-7-button"
-                        >
-                            Add to Cart
-                        </button>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="image-box">
-                        <div className="images" id="image-8" />
-                    </div>
-                    <div className="text-box">
-                        <h2 className="item">Plum</h2>
-                        <h3 className="price">$4.99</h3>
-                        <p className="description">A bag of delicious plums!</p>
-                        <label htmlFor="item-8-quantity">Quantity:</label>
-                        <input
-                            type="text"
-                            name="item-8-quantity"
-                            id="item-8-quantity"
-                            defaultValue={1}
-                        />
-                        <button
-                            type="button"
-                            name="item-8-button"
-                            id="item-8-button"
-                        >
-                            Add to Cart
-                        </button>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="image-box">
-                        <div className="images" id="image-9" />
-                    </div>
-                    <div className="text-box">
-                        <h2 className="item">Avocado</h2>
-                        <h3 className="price">$4.99</h3>
-                        <p className="description">
-                            A bag of delicious avocados!
-                        </p>
-                        <label htmlFor="item-9-quantity">Quantity:</label>
-                        <input
-                            type="text"
-                            name="item-9-quantity"
-                            id="item-9-quantity"
-                            defaultValue={1}
-                        />
-                        <button
-                            type="button"
-                            name="item-9-button"
-                            id="item-9-button"
-                        >
-                            Add to Cart
-                        </button>
-                    </div>
-                </div>
-                <div className="product">
-                    <div className="image-box">
-                        <div className="images" id="image-10" />
-                    </div>
-                    <div className="text-box">
-                        <h2 className="item">Lemon</h2>
-                        <h3 className="price">$4.99</h3>
-                        <p className="description">
-                            A bag of delicious lemons!
-                        </p>
-                        <label htmlFor="item-10-quantity">Quantity:</label>
-                        <input
-                            type="text"
-                            name="item-10-quantity"
-                            id="item-10-quantity"
-                            defaultValue={1}
-                        />
-                        <button
-                            type="button"
-                            name="item-10-button"
-                            id="item-10-button"
-                        >
-                            Add to Cart
-                        </button>
-                    </div>
+                <div className={styles["text-box"]}>
+                    <h2 className="item">Orange</h2>
+                    <h3 className="price">$4.99</h3>
+                    <p className="description">A bag of delicious oranges!</p>
+                    <button type="button" name="item-1-button">
+                        Details
+                    </button>
                 </div>
             </div>
-        </>
+        </section>
+        // </main>
     );
 };
