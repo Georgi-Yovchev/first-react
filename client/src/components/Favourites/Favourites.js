d106ebea20ccb72e0cc7f361a20720389276ea76
Georgi-Yovchev <130495193+Georgi-Yovchev@users.noreply.github.com>
2023-04-16 20:02:47 +0300

    fixed warnings

--- client/src/components/Favourites/Favourites.js
+++ client/src/components/Favourites/Favourites.js
@@ -1,4 +1,3 @@
-import { CarContext } from "../../contexts/CarContext";
 import { List } from "../common/List/List";
 
 import { useContext, useEffect, useState } from "react";
@@ -6,14 +5,14 @@ import { AuthContext } from "../../contexts/AuthContext";
 import * as favouriteService from "../../services/favouriteService";
 
 export const Favourites = () => {
-    const { userId, isAuthenticated, auth, username } = useContext(AuthContext);
+    const { userId } = useContext(AuthContext);
     const [favCars, setFavCars] = useState([]);
 
     useEffect(() => {
         favouriteService.checkByUserId(userId).then((result) => {
             setFavCars(result);
         });
-    }, []);
+    }, [userId]);
 
     return <List {...favCars} />;
 };
