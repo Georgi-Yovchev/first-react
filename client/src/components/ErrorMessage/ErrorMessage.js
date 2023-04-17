66a592fe94033bab1b520a94a197c81b14536fd0
Georgi-Yovchev <130495193+Georgi-Yovchev@users.noreply.github.com>
2023-04-17 18:45:24 +0300

    make ErrorMessage dissapear

--- client/src/components/ErrorMessage/ErrorMessage.js
+++ client/src/components/ErrorMessage/ErrorMessage.js
@@ -1,10 +1,17 @@
 import styles from "./ErrorMessage.module.css";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 import { ErrorContext } from "../../contexts/ErrorContext";
 
 export const ErrorMessage = () => {
     const { err, setErr } = useContext(ErrorContext);
+
+    if (err) {
+        setTimeout(() => {
+            setErr("");
+            console.log("hmm");
+        }, 5000);
+    }
 
     return (
         <div className={err ? styles["error-box"] : styles["hidden"]}>
